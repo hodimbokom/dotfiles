4366e6fd251151d68d8cabe8aa0573d0b8cf3693
rukipomoi <rukipomoi@gmail.com>
2024-08-17 03:36:08 +0400

    upd

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,63 +1,62 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.7",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
-      },
-    },
-    opts = {
-      extensions_list = { "fzf" },
-    },
-    config = function()
-      require("telescope").setup {
-        defaults = {
-          dynamic_preview_title = true,
-          results_title = false,
-          path_display = { "truncate" },
-          file_ignore_patters = { "node_modules", ".git/" },
-          layout_strategy = "vertical",
-          layout_config = {
-            vertical = {
-              height = 0.8,
-              width = 0.8,
-              mirror = false,
-              prompt_position = "bottom",
-              preview_cutoff = 20,
-            },
-          },
-        },
-        pickers = {
-          find_files = {
-            prompt_title = false,
-            hidden = true,
-            find_command = {
-              "rg",
-              "--files",
-              "--hidden",
-             "--glob=!**/dist/*",
-             "--glob=!**/build/*",
-             "--glob=!**/.git/*",
-           }
-          },
-          git_files = { prompt_title = false, hidden = true },
-        },
-      }
-      local builtin = require('telescope.builtin')
+	{
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.7',
+		dependencies = {
+			'nvim-lua/plenary.nvim',
+			{
+				'nvim-telescope/telescope-fzf-native.nvim',
+				build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
+			},
+		},
+		opts = {
+			extensions_list = { 'fzf' },
+		},
+		config = function()
+			require('telescope').setup({
+				defaults = {
+					dynamic_preview_title = true,
+					results_title = false,
+					path_display = { 'truncate' },
+					file_ignore_patters = { 'node_modules', '.git/' },
+					layout_strategy = 'vertical',
+					layout_config = {
+						vertical = {
+							height = 0.8,
+							width = 0.8,
+							mirror = false,
+							prompt_position = 'bottom',
+							preview_cutoff = 20,
+						},
+					},
+				},
+				pickers = {
+					find_files = {
+						prompt_title = false,
+						hidden = true,
+						find_command = {
+							'rg',
+							'--files',
+							'--hidden',
+							'--glob=!**/dist/*',
+							'--glob=!**/build/*',
+							'--glob=!**/.git/*',
+						},
+					},
+					git_files = { prompt_title = false, hidden = true },
+				},
+			})
+			local builtin = require('telescope.builtin')
 
-      vim.keymap.set({'n', 'i', 'v'}, '<C-p>', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>f', builtin.git_files, {})
-      vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
+			vim.keymap.set({ 'n', 'i', 'v' }, '<C-p>', builtin.find_files, {})
+			vim.keymap.set('n', '<leader>f', builtin.git_files, {})
+			vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
 
-      vim.keymap.set('n', '<leader>ps', function()
-        builtin.grep_string({ search = vim.fn.input("Grep > ") })
-      end)
+			vim.keymap.set('n', '<leader>ps', function()
+				builtin.grep_string({ search = vim.fn.input('Grep > ') })
+			end)
 
-      require("telescope").load_extension("fzf")
-    end
-  },
+			require('telescope').load_extension('fzf')
+		end,
+	},
 }
-
