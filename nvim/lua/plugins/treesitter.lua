2d2d9bf1c08cf8406dc47bea2630c8520fad173c
rukipomoi <rukipomoi@gmail.com>
2025-09-03 14:41:05 +0400

    Limit starship modules scan

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,34 +1,34 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local configs = require("nvim-treesitter.configs")
+	{
+		'nvim-treesitter/nvim-treesitter',
+		build = ':TSUpdate',
+		config = function()
+			local configs = require('nvim-treesitter.configs')
 
-      configs.setup {
-	      ensure_installed = {
-          "bash",
-          "lua",
-          "javascript",
-          "typescript",
-          "c",
-          "vimdoc",
-          "python",
-          "go",
-          "rust",
-          "css",
-          "html",
-          "toml",
-          "ini",
-          "yaml",
-        },
-        sync_install = false,
-	      auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
+			configs.setup({
+				ensure_installed = {
+					'bash',
+					'lua',
+					'javascript',
+					'typescript',
+					'c',
+					'vimdoc',
+					'python',
+					'go',
+					'rust',
+					'css',
+					'html',
+					'toml',
+					'ini',
+					'yaml',
+				},
+				sync_install = false,
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
 
-        vim.cmd [[autocmd BufNewFile,BufRead *.conf set filetype=tmux]]
-      }
-    end
-  }
+				vim.cmd([[autocmd BufNewFile,BufRead *.conf set filetype=tmux]]),
+			})
+		end,
+	},
 }
