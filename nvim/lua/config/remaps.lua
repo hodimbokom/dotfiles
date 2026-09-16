695e8a1d8ea325ba93e8ad44fc00624c5f593e7f
rukipomoi <rukipomoi@gmail.com>
2026-09-16 20:33:01 +0400

    Replace fugitive with lazygit

--- nvim/lua/config/remaps.lua
+++ nvim/lua/config/remaps.lua
@@ -6,3 +6,24 @@ vim.keymap.set("n", "<leader>w", "<cmd>w<cr><esc>")
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("n", "<leader>gg", function()
+	if not vim.env.TMUX then
+		vim.cmd("tabnew | terminal lazygit")
+		return
+	end
+	vim.fn.jobstart({
+		"tmux",
+		"display-popup",
+		"-d",
+		vim.fn.getcwd(),
+		"-E",
+		"-h",
+		"90%",
+		"-w",
+		"90%",
+		"-T",
+		"lazygit",
+		"lazygit",
+	}, { detach = true })
+end)
--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -2,6 +2,7 @@ return {
 	'lewis6991/gitsigns.nvim',
 	config = function()
 		require('gitsigns').setup({
+			signs_staged_enable = true,
 			on_attach = function(bufnr)
 				local gitsigns = require('gitsigns')
 
@@ -27,23 +28,11 @@ return {
 					end
 				end)
 
-				map('n', '<leader>hs', gitsigns.stage_hunk)
-				map('n', '<leader>hr', gitsigns.reset_hunk)
-				map('v', '<leader>hs', function()
-					gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
-				end)
-				map('v', '<leader>hr', function()
-					gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
-				end)
-				map('n', '<leader>hS', gitsigns.stage_buffer)
-				map('n', '<leader>hu', gitsigns.undo_stage_hunk)
-				map('n', '<leader>hR', gitsigns.reset_buffer)
 				map('n', '<leader>hp', gitsigns.preview_hunk)
 				map('n', '<leader>hb', function()
 					gitsigns.blame_line({ full = true })
 				end)
 				map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
-				map('n', '<leader>hd', gitsigns.diffthis)
 				map('n', '<leader>td', gitsigns.toggle_deleted)
 			end,
 		})
